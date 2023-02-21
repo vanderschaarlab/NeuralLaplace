ce2e150f90abcad8ae91f9f720b88d6896fd5be1
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-02-21 08:44:57 +0000

    cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 import datetime
-import sys
 import os
+import sys
 
 sys.path.insert(0, os.path.abspath("../../"))
 
@@ -12,9 +12,9 @@ from torchlaplace import __version__
 now = datetime.datetime.now()
 
 
-project = 'TorchLaplace'
-author = 'Sam Holt'
-copyright = f'{now.year}, {author}'
+project = "TorchLaplace"
+author = "Sam Holt"
+copyright = f"{now.year}, {author}"
 
 version = __version__
 release = version
@@ -22,32 +22,32 @@ release = version
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx_panels',
-    'nbsphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx_panels",
+    "nbsphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 html_css_files = [
     "torchlaplace.css",
 ]
 
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
